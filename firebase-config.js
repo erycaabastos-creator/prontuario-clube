86914e7b6896009ebd0797978968013d68409d6f
erycaabastos-creator <erycaabastos@gmail.com>
2026-09-16 16:58:17 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// PREENCHA com as credenciais do SEU projeto Firebase novo (separado do Baú e do particular).
+// Você encontra esses valores em: Firebase Console > Configurações do projeto > Seus apps > SDK setup.
+// Depois de preencher, esse mesmo arquivo é importado por todas as páginas do sistema.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD3RVMKSUqSCRUFiAP8vGskbxnnZxjsjjs",
+  authDomain: "psicologia-cec.firebaseapp.com",
+  projectId: "psicologia-cec",
+  storageBucket: "psicologia-cec.firebasestorage.app",
+  messagingSenderId: "323703399364",
+  appId: "1:323703399364:web:ae4821023a4735e1c33a91"
+};
